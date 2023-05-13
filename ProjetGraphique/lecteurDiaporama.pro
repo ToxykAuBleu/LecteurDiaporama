da4b88644bbe16daf75048a11b7de7a0f7b33686
ToxykAuBleu <foissac.mathieu@gmail.com>
2023-05-13 11:44:47 +0200

    Bug fix: correction du merge a753d3673ea4507f3edceafa4ad731469c16f878

--- ProjetGraphique/lecteurDiaporama.pro
+++ ProjetGraphique/lecteurDiaporama.pro
@@ -9,10 +9,14 @@ CONFIG += c++17
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    image.cpp \
+    lecteur.cpp \
     main.cpp \
     lecteurvue.cpp
 
 HEADERS += \
+    image.h \
+    lecteur.h \
     lecteurvue.h
 
 FORMS += \
@@ -22,3 +26,6 @@ FORMS += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    Images.qrc
--- ProjetGraphique/lecteurvue.h
+++ ProjetGraphique/lecteurvue.h
@@ -1,11 +1,8 @@
 #ifndef LECTEURVUE_H
 #define LECTEURVUE_H
-<<<<<<< HEAD
-#include <QMessageBox>
-=======
 
+#include <QMessageBox>
 #include "lecteur.h"
->>>>>>> d92b7c9cb8cc0f8a5a80a7a2161ab1286371475c
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -27,7 +24,6 @@ public slots:
     void demarrerDiapo(); // Passe le diaporama en mode automatique
     void arreterDiapo(); // Passe le diaporama en mode manuel
     void apropos(); //Ouvre boite de dialogue contenant les informations sur le diapo
-    void quitter(); // Quitte le diaporama
     void afficherImageCourante(); // Affiche l'image courant du lecteur sur l'interface graphique
 
 private:
@@ -35,9 +31,6 @@ private:
     const std::string version = "v2";
     const std::string auteurs = "Virgile, Mathieu, Adrien";
     const std::string date = "12/05/2023";
-
-private:
-    Ui::LecteurVue *ui;
     Lecteur _lecteur;
 };
 #endif // LECTEURVUE_H
